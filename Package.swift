3f0aeff1ef3aaf1410d28602589ea741254edb69
Michal Tomlein <michal@in2core.com>
2023-01-11 16:14:52 +0100

    Fix symlinks in framework

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScreenPortSDK",
-            url: "https://github.com/in2core/screenportsdk/releases/download/v0.2.0-alpha.1/ScreenPortSDK-0.2.0-alpha.1.zip",
-            checksum: "08393c06714d6cf4da312bbf5c15d07cc54e8d43ed385d634bd5063ffc478afc"),
+            url: "https://github.com/in2core/screenportsdk/releases/download/v0.2.0-alpha.2/ScreenPortSDK-0.2.0-alpha.2.zip",
+            checksum: "e23f582983a5556ff2b09c80241b9495f0fac5cce044d452053d2ac81b84fb05"),
     ]
 )
